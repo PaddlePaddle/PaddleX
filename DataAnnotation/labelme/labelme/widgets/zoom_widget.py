99212056b287579b13eacbe0ae9a6fbe23a3f1ea
LaraStuStu <lvxueying1991@foxmail.com>
2020-03-28 14:02:03 +0800

    Create zoom_widget.py

---
+++ DataAnnotation/labelme/labelme/widgets/zoom_widget.py
@@ -0,0 +1,22 @@
+from qtpy import QtCore
+from qtpy import QtGui
+from qtpy import QtWidgets
+
+
+class ZoomWidget(QtWidgets.QSpinBox):
+
+    def __init__(self, value=100):
+        super(ZoomWidget, self).__init__()
+        self.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.setRange(10, 1000)
+        self.setSuffix(' %')
+        self.setValue(value)
+        self.setToolTip('Zoom Level')
+        self.setStatusTip(self.toolTip())
+        self.setAlignment(QtCore.Qt.AlignCenter)
+
+    def minimumSizeHint(self):
+        height = super(ZoomWidget, self).minimumSizeHint().height()
+        fm = QtGui.QFontMetrics(self.font())
+        width = fm.width(str(self.maximum()))
+        return QtCore.QSize(width, height)
