5cbf7e3f57ab760ab86da01a2192ddd6e9fc9f85
laibaohua <laibaohua@baidu.com>
2020-03-28 16:09:41 +0800

    update documents

--- DataAnnotation/labelme/github2pypi/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-
-from .replace_url import replace_url
--- DataAnnotation/labelme/labelme/widgets/escapable_qlist_widget.py
+++
@@ -1,9 +0,0 @@
-from qtpy.QtCore import Qt
-from qtpy import QtWidgets
-
-
-class EscapableQListWidget(QtWidgets.QListWidget):
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Escape:
-            self.clearSelection()
