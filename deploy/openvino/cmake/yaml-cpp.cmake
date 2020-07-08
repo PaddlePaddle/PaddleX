ff4f0bac5fe11ca285271c5d6026ca5f2fc47772
syyxsxx <csuwsy@163.com>
2020-07-08 11:19:11 +0000

    change yml download

--- deploy/openvino/cmake/yaml-cpp.cmake
+++ deploy/openvino/cmake/yaml-cpp.cmake
@@ -6,9 +6,8 @@ message("${CMAKE_BUILD_TYPE}")
 
 ExternalProject_Add(
         ext-yaml-cpp
-        #URL https://bj.bcebos.com/paddlex/deploy/deps/yaml-cpp.zip
-        #URL_MD5 9542d6de397d1fbd649ed468cb5850e6
-        URL "/wangsiyuan06/PaddleX/deploy/openvino/deps/yaml-cpp.zip"
+        URL https://bj.bcebos.com/paddlex/deploy/deps/yaml-cpp.zip
+        URL_MD5 9542d6de397d1fbd649ed468cb5850e6
         CMAKE_ARGS
         -DYAML_CPP_BUILD_TESTS=OFF
         -DYAML_CPP_BUILD_TOOLS=OFF
