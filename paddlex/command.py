d57a0323044de6a8a569240d9abda2f84a738ba7
SunAhong1993 <sunyanfang01@baidu.com>
2020-07-08 15:30:46 +0800

    add jingling convert

--- paddlex/command.py
+++ paddlex/command.py
@@ -51,6 +51,36 @@ def arg_parser():
         default=False,
         help="export onnx model for deployment")
     parser.add_argument(
+        "--data_conversion",
+        "-dc",
+        action="store_true",
+        default=False,
+        help="convert the dataset to the standard format")
+    parser.add_argument(
+        "--from_ds",
+        "-fd",
+        type=_text_type,
+        default=None,
+        help="define dataset format before the conversion")
+    parser.add_argument(
+        "--to_ds",
+        "-td",
+        type=_text_type,
+        default=None,
+        help="define dataset format after the conversion")
+    parser.add_argument(
+        "--pics",
+        "-p",
+        type=_text_type,
+        default=None,
+        help="define pictures directory path")
+    parser.add_argument(
+        "--annotations",
+        "-a",
+        type=_text_type,
+        default=None,
+        help="define annotations directory path")
+    parser.add_argument(
         "--fixed_input_shape",
         "-fs",
         default=None,
@@ -105,6 +135,20 @@ def main():
                 "paddlex --export_inference --model_dir model_path --save_dir infer_model"
             )
         pdx.convertor.export_onnx_model(model, args.save_dir)
+        
+    if args.data_conversion:
+        assert args.from_ds is not None, "--from_ds should be defined while converting dataset"
+        assert args.to_ds is not None, "--to_ds should be defined to confirm the taregt dataset format"
+        assert args.pics is not None, "--pics should be defined to confirm the pictures path"
+        assert args.annotations is not None, "--annotations should be defined to confirm the annotations path"
+        assert args.save_dir is not None, "--save_dir should be defined to store taregt dataset"
+        if args.from_ds == 'labelme' and args.to_ds == 'ImageNet':
+            logging.error(
+                "The labelme dataset can not convert to the ImageNet dataset.",
+                exit=False)
+        pdx.tools.convert.dataset_conversion(args.from_ds, args.to_ds, 
+                                             args.pics, args.annotations, args.save_dir )
+        
 
 
 if __name__ == "__main__":
--- paddlex/tools/convert.py
+++ paddlex/tools/convert.py
@@ -15,8 +15,10 @@
 # limitations under the License.
 
 from .x2imagenet import EasyData2ImageNet
+from .x2imagenet import JingLing2ImageNet
 from .x2coco import LabelMe2COCO
 from .x2coco import EasyData2COCO
+from .x2coco import JingLing2COCO
 from .x2voc import LabelMe2VOC
 from .x2voc import EasyData2VOC
 from .x2seg import JingLing2Seg
@@ -24,10 +26,34 @@ from .x2seg import LabelMe2Seg
 from .x2seg import EasyData2Seg
 
 easydata2imagenet = EasyData2ImageNet().convert
+jingling2imagenet = JingLing2ImageNet().convert
 labelme2coco = LabelMe2COCO().convert
 easydata2coco = EasyData2COCO().convert
+jingling2coco = JingLing2COCO().convert
 labelme2voc = LabelMe2VOC().convert
 easydata2voc = EasyData2VOC().convert
 jingling2seg = JingLing2Seg().convert
 labelme2seg = LabelMe2Seg().convert
 easydata2seg = EasyData2Seg().convert
+
+def dataset_conversion(from_ds, to_ds, pics, anns, save_dir):
+    if from_ds == 'labelme' and to_ds == 'PascalVOC':
+        labelme2voc(pics, anns, save_dir)
+    elif from_ds == 'labelme' and to_ds == 'MSCOCO':
+        labelme2coco(pics, anns, save_dir)
+    elif from_ds == 'labelme' and to_ds == 'SEG':
+        labelme2seg(pics, anns, save_dir)
+    elif from_ds == 'jingling' and to_ds == 'ImageNet':
+        jingling2imagenet(pics, anns, save_dir)
+    elif from_ds == 'jingling' and to_ds == 'MSCOCO':
+        jingling2coco(pics, anns, save_dir)
+    elif from_ds == 'jingling' and to_ds == 'SEG':
+        jingling2seg(pics, anns, save_dir)
+    elif from_ds == 'easydata' and to_ds == 'ImageNet':
+        easydata2imagenet(pics, anns, save_dir)
+    elif from_ds == 'easydata' and to_ds == 'PascalVOC':
+        easydata2voc(pics, anns, save_dir)
+    elif from_ds == 'easydata' and to_ds == 'MSCOCO':
+        easydata2coco(pics, anns, save_dir)
+    elif from_ds == 'easydata' and to_ds == 'SEG':
+        easydata2seg(pics, anns, save_dir)
