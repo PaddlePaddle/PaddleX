009e1ee767779e817887f9bd1184a6bed7126461
FlyingQianMM <245467267@qq.com>
2020-07-20 19:49:32 +0800

    fix error when decompress opencv for jetson

--- deploy/cpp/scripts/jetson_bootstrap.sh
+++ deploy/cpp/scripts/jetson_bootstrap.sh
@@ -4,7 +4,7 @@ if [ ! -d "./deps/opencv3" ]; then
     mkdir -p deps
     cd deps
     wget -c ${OPENCV_URL}
-    tar xvfj opencv3_aarch.tgz
+    tar xvfz opencv3_aarch.tgz
     rm -rf opencv3_aarch.tgz
     cd ..
 fi
