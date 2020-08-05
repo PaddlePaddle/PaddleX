a237df88ef9c5a85c264f8e499dd63c086bc77a5
mamingjie-China <mamingjie@baidu.com>
2020-08-05 17:30:58 +0800

    add tools/split.py

--- paddlex/tools/split.py
+++ paddlex/tools/split.py
@@ -14,5 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .convert import *
-from .split import *
+
+def dataset_split(dataset_dir, dataset_form, val_value, test_value, save_dir):
+    print(dataset_dir, dataset_form, val_value, test_value, save_dir)
+    print(12345)
