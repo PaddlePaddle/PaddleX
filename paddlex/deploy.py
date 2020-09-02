4fbd8f6fb61e1b4b48719aba3717423e8709298b
FlyingQianMM <245467267@qq.com>
2020-09-02 10:06:25 +0800

    set use_mkl to True in deploy.py

--- paddlex/deploy.py
+++ paddlex/deploy.py
@@ -30,7 +30,7 @@ class Predictor:
                  model_dir,
                  use_gpu=True,
                  gpu_id=0,
-                 use_mkl=False,
+                 use_mkl=True,
                  mkl_thread_num=4,
                  use_trt=False,
                  use_glog=False,
@@ -94,7 +94,7 @@ class Predictor:
                          use_gpu=True,
                          gpu_id=0,
                          use_mkl=False,
-                         mkl_thread_num=mp.cpu_count(),
+                         mkl_thread_num=4,
                          use_trt=False,
                          use_glog=False,
                          memory_optimize=True):
