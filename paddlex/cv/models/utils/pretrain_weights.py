2ef5e7aca851e0e2c04806f061662ef689806a95
jiangjiajun <jiangjiajun@baidu.com>
2020-09-28 03:33:08 +0000

    add gui_mode

--- paddlex/cv/models/utils/pretrain_weights.py
+++ paddlex/cv/models/utils/pretrain_weights.py
@@ -202,11 +202,11 @@ def get_pretrain_weights(flag, class_name, backbone, save_dir):
         assert backbone in image_pretrain, "There is not ImageNet pretrain weights for {}, you may try COCO.".format(
             backbone)
 
-        #        if backbone == 'AlexNet':
-        #            url = image_pretrain[backbone]
-        #            fname = osp.split(url)[-1].split('.')[0]
-        #            paddlex.utils.download_and_decompress(url, path=new_save_dir)
-        #            return osp.join(new_save_dir, fname)
+        if getattr(paddlex, 'gui_mode', False):
+            url = image_pretrain[backbone]
+            fname = osp.split(url)[-1].split('.')[0]
+            paddlex.utils.download_and_decompress(url, path=new_save_dir)
+            return osp.join(new_save_dir, fname)
         try:
             logging.info(
                 "Connecting PaddleHub server to get pretrain weights...")
@@ -241,8 +241,11 @@ def get_pretrain_weights(flag, class_name, backbone, save_dir):
         elif flag == 'CITYSCAPES':
             url = cityscapes_pretrain[backbone]
         fname = osp.split(url)[-1].split('.')[0]
-        #        paddlex.utils.download_and_decompress(url, path=new_save_dir)
-        #        return osp.join(new_save_dir, fname)
+
+        if getattr(paddlex, 'gui_mode', False):
+            paddlex.utils.download_and_decompress(url, path=new_save_dir)
+            return osp.join(new_save_dir, fname)
+
         try:
             logging.info(
                 "Connecting PaddleHub server to get pretrain weights...")
