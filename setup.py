b054bd212f19aecfbf07e92d501b98dfe003cb82
jiangjiajun <jiangjiajun@baidu.com>
2020-10-26 09:16:03 +0000

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = "PaddlePaddle Entire Process Development Toolkit"
 
 setuptools.setup(
     name="paddlex",
-    version='1.2.1',
+    version='1.2.2',
     author="paddlex",
     author_email="paddlex@baidu.com",
     description=long_description,
