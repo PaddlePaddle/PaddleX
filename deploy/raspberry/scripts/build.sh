362e2031d21f42fa356baf6619c83d8b64c00d71
syyxsxx <32666364+syyxsxx@users.noreply.github.com>
2020-12-23 19:23:49 +0800

    Update build.sh

--- deploy/raspberry/scripts/build.sh
+++ deploy/raspberry/scripts/build.sh
@@ -7,7 +7,7 @@ GFLAGS_DIR=$(pwd)/deps/gflags
 # opencv预编译库的路径, 如果使用自带预编译版本可不修改
 OPENCV_DIR=$(pwd)/deps/opencv
 
-# arm处理器架构，默认为armv7若CPU为armv8请修改为ARCH=armv8
+# arm处理器架构，默认为armv7下ARCH=armv7-a，若CPU为armv8请修改为ARCH=armv8-a
 ARCH=armv7-a
 
 # 采用lite的版本，可选为full 与 light，默认为full版
