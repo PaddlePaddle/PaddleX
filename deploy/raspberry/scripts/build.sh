faf2e5ebd66fc3a5fc565a5cc95e44b9b4d1d499
syyxsxx <csuwsy@163.com>
2020-12-23 16:51:07 +0800

    fix rasp

--- deploy/raspberry/scripts/build.sh
+++ deploy/raspberry/scripts/build.sh
@@ -8,7 +8,7 @@ GFLAGS_DIR=$(pwd)/deps/gflags
 OPENCV_DIR=$(pwd)/deps/opencv
 
 # arm处理器架构，默认为armv7若CPU为armv8请修改为ARCH=armv8
-ARCH=armv7
+ARCH=armv7-a
 
 # 采用lite的版本，可选为full 与 light，默认为full版
 LITE=full
@@ -24,5 +24,6 @@ cmake .. \
     -DGFLAGS_DIR=${GFLAGS_DIR} \
     -DLITE_DIR=${LITE_DIR} \
     -DARCH=${ARCH} \
-    -DLITE=${LITE}
+    -DLITE=${LITE} \
+    -DCMAKE_CXX_FLAGS="-march=${ARCH}"
 make -j4
