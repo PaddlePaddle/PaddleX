3e5010c67ddd55d77c38be261894f5089f9d9b8a
jiangjiajun <jiangjiajun@baidu.com>
2020-12-25 07:40:48 +0000

    update version

--- paddlex/__init__.py
+++ paddlex/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '1.2.6'
+__version__ = '1.3.1'
 
 import os
 if 'FLAGS_eager_delete_tensor_gb' not in os.environ:
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = "PaddlePaddle Entire Process Development Toolkit"
 
 setuptools.setup(
     name="paddlex",
-    version='1.2.6',
+    version='1.3.1',
     author="paddlex",
     author_email="paddlex@baidu.com",
     description=long_description,
