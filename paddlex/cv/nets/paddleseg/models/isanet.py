d0d258da8766f684ab7f4b3727099041435dc0ee
will-jl944 <lujiafeng@baidu.com>
2021-03-29 22:14:05 +0800

    keep up with seg release/2.0

--- paddlex/cv/nets/paddleseg/models/isanet.py
+++ paddlex/cv/nets/paddleseg/models/isanet.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
-
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
@@ -72,7 +70,7 @@ class ISANet(nn.Layer):
         logit_list = [
             F.interpolate(
                 logit,
-                x.shape[2:],
+                paddle.shape(x)[2:],
                 mode='bilinear',
                 align_corners=self.align_corners,
                 align_mode=1) for logit in logit_list
@@ -102,6 +100,7 @@ class ISAHead(nn.Layer):
         super(ISAHead, self).__init__()
         self.in_channels = in_channels[-1]
         inter_channels = self.in_channels // 4
+        self.inter_channels = inter_channels
         self.down_factor = down_factor
         self.enable_auxiliary_loss = enable_auxiliary_loss
         self.in_conv = layers.ConvBNReLU(
@@ -124,32 +123,42 @@ class ISAHead(nn.Layer):
     def forward(self, feat_list):
         C3, C4 = feat_list
         x = self.in_conv(C4)
-        n, c, h, w = x.shape
+        x_shape = paddle.shape(x)
         P_h, P_w = self.down_factor
-        Q_h, Q_w = math.ceil(h / P_h), math.ceil(w / P_w)
-        pad_h, pad_w = Q_h * P_h - h, Q_w * P_w - w
+        Q_h, Q_w = paddle.ceil(x_shape[2] / P_h).astype('int32'), paddle.ceil(
+            x_shape[3] / P_w).astype('int32')
+        pad_h, pad_w = (Q_h * P_h - x_shape[2]).astype('int32'), (
+            Q_w * P_w - x_shape[3]).astype('int32')
         if pad_h > 0 or pad_w > 0:
-            padding = [
-                pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2
-            ]
+            padding = paddle.concat(
+                [
+                    pad_w // 2, pad_w - pad_w // 2, pad_h // 2,
+                    pad_h - pad_h // 2
+                ],
+                axis=0)
             feat = F.pad(x, padding)
         else:
             feat = x
 
-        feat = feat.reshape([n, c, Q_h, P_h, Q_w, P_w])
-        feat = feat.transpose([0, 3, 5, 1, 2, 4]).reshape([-1, c, Q_h, Q_w])
+        feat = feat.reshape([0, x_shape[1], Q_h, P_h, Q_w, P_w])
+        feat = feat.transpose(
+            [0, 3, 5, 1, 2, 4]).reshape([-1, self.inter_channels, Q_h, Q_w])
         feat = self.global_relation(feat)
 
-        feat = feat.reshape([n, P_h, P_w, c, Q_h, Q_w])
-        feat = feat.transpose([0, 4, 5, 3, 1, 2]).reshape([-1, c, P_h, P_w])
+        feat = feat.reshape([x_shape[0], P_h, P_w, x_shape[1], Q_h, Q_w])
+        feat = feat.transpose(
+            [0, 4, 5, 3, 1, 2]).reshape([-1, self.inter_channels, P_h, P_w])
         feat = self.local_relation(feat)
 
-        feat = feat.reshape([n, Q_h, Q_w, c, P_h, P_w])
+        feat = feat.reshape([x_shape[0], Q_h, Q_w, x_shape[1], P_h, P_w])
         feat = feat.transpose([0, 3, 1, 4, 2, 5]).reshape(
-            [n, c, P_h * Q_h, P_w * Q_w])
+            [0, self.inter_channels, P_h * Q_h, P_w * Q_w])
         if pad_h > 0 or pad_w > 0:
-            feat = feat[:, :, pad_h // 2:pad_h // 2 + h, pad_w // 2:pad_w // 2
-                        + w]
+            feat = paddle.slice(
+                feat,
+                axes=[2, 3],
+                starts=[pad_h // 2, pad_w // 2],
+                ends=[pad_h // 2 + x_shape[2], pad_w // 2 + x_shape[3]])
 
         feat = self.out_conv(paddle.concat([feat, x], axis=1))
         output = self.cls(feat)
