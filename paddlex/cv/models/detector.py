41e34c899547fffdb5c123cfcf0f8c9bc301f6b7
will-jl944 <lujiafeng@baidu.com>
2021-04-20 17:57:30 +0800

    add **params to _get_backbone

--- paddlex/cv/models/detector.py
+++ paddlex/cv/models/detector.py
@@ -59,27 +59,17 @@ class BaseDetector(BaseModel):
         ]
         return net, test_inputs
 
-    def _get_backbone(self, backbone_name):
+    def _get_backbone(self, backbone_name, **params):
         if backbone_name == 'MobileNetV1':
-            backbone = backbones.MobileNet()
+            backbone = backbones.MobileNet(**params)
         elif backbone_name == 'MobileNetV3':
-            backbone = backbones.MobileNetV3(feature_maps=[7, 13, 16])
+            backbone = backbones.MobileNetV3(**params)
         elif backbone_name == 'DarkNet53':
-            backbone = backbones.DarkNet()
+            backbone = backbones.DarkNet(**params)
         elif backbone_name == 'ResNet50_vd':
-            backbone = backbones.ResNet(
-                variant='d',
-                return_idx=[1, 2, 3],
-                dcn_v2_stages=[-1],
-                freeze_at=-1,
-                freeze_norm=False)
+            backbone = backbones.ResNet(**params)
         elif backbone_name == 'ResNet50_vd_dcn':
-            backbone = backbones.ResNet(
-                variant='d',
-                return_idx=[1, 2, 3],
-                dcn_v2_stages=[3],
-                freeze_at=-1,
-                freeze_norm=False)
+            backbone = backbones.ResNet(**params)
         else:
             raise ValueError("There is no backbone for {} named {}".format(
                 self.__class__.__name__, backbone_name))
@@ -374,7 +364,7 @@ class YOLOv3(BaseDetector):
             self.sync_bn = False
 
         self.backbone_name = backbone
-        backbone = self._get_backbone(backbone)
+        backbone = self._get_backbone(backbone, **params)
         neck = necks.YOLOv3FPN()
         loss = losses.YOLOv3Loss(
             num_classes=num_classes,
