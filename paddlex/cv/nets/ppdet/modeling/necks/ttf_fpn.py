a32146f084946cae61271f06bb10995c9567b4b4
will-jl944 <lujiafeng@baidu.com>
2021-04-25 19:59:37 +0800

    keep up with ppdet

--- paddlex/cv/nets/ppdet/modeling/necks/ttf_fpn.py
+++ paddlex/cv/nets/ppdet/modeling/necks/ttf_fpn.py
@@ -18,7 +18,7 @@ import paddle.nn.functional as F
 from paddle import ParamAttr
 from paddle.nn.initializer import Constant, Uniform, Normal, XavierUniform
 from paddle import ParamAttr
-from ppdet.core.workspace import register, serializable
+from paddlex.cv.nets.ppdet.core.workspace import register, serializable
 from paddle.regularizer import L2Decay
 from paddlex.cv.nets.ppdet.modeling.layers import DeformableConvV2, ConvNormLayer, LiteConv
 import math
