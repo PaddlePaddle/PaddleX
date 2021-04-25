0d9b0790dd96a026734a3f14f570598dcbd1f3b1
will-jl944 <lujiafeng@baidu.com>
2021-04-25 17:09:25 +0800

    add tutorial and pretiained weights for ppyolov2

--- paddlex/utils/checkpoint.py
+++ paddlex/utils/checkpoint.py
@@ -41,7 +41,9 @@ det_pretrain_weights_dict = {
     'FasterRCNN_ResNet101_fpn': ['COCO', 'IMAGENET'],
     'FasterRCNN_ResNet101_vd_fpn': ['COCO', 'IMAGENET'],
     'PPYOLO_ResNet50_vd_dcn': ['COCO', 'IMAGENET'],
-    'PPYOLO_ResNet18_vd': ['COCO', 'IMAGENET']
+    'PPYOLO_ResNet18_vd': ['COCO', 'IMAGENET'],
+    'PPYOLOv2_ResNet50_vd_dcn': ['COCO', 'IMAGENET'],
+    'PPYOLOv2_ResNet101_vd_dcn': ['COCO', 'IMAGENET']
 }
 
 cityscapes_weights = {
@@ -201,7 +203,11 @@ imagenet_weights = {
     'PPYOLO_ResNet50_vd_dcn_IMAGENET':
     'https://paddledet.bj.bcebos.com/models/pretrained/ResNet50_vd_ssld_pretrained.pdparams',
     'PPYOLO_ResNet18_vd_IMAGENET':
-    'https://paddledet.bj.bcebos.com/models/pretrained/ResNet18_vd_pretrained.pdparams'
+    'https://paddledet.bj.bcebos.com/models/pretrained/ResNet18_vd_pretrained.pdparams',
+    'PPYOLOv2_ResNet50_vd_dcn_IMAGENET':
+    'https://paddledet.bj.bcebos.com/models/pretrained/ResNet50_vd_ssld_pretrained.pdparams',
+    'PPYOLOv2_ResNet101_vd_dcn_IMAGENET':
+    'https://paddledet.bj.bcebos.com/models/pretrained/ResNet101_vd_ssld_pretrained.pdparams'
 }
 
 pascalvoc_weights = {
@@ -249,7 +255,11 @@ coco_weights = {
     'PPYOLO_ResNet50_vd_dcn_COCO':
     'https://paddledet.bj.bcebos.com/models/ppyolo_r50vd_dcn_2x_coco.pdparams',
     'PPYOLO_ResNet18_vd_COCO':
-    'https://paddledet.bj.bcebos.com/models/ppyolo_r18vd_coco.pdparams'
+    'https://paddledet.bj.bcebos.com/models/ppyolo_r18vd_coco.pdparams',
+    'PPYOLOv2_ResNet50_vd_dcn_COCO':
+    'https://paddledet.bj.bcebos.com/models/ppyolov2_r50vd_dcn_365e_coco.pdparams',
+    'PPYOLOv2_ResNet101_vd_dcn_COCO':
+    'https://paddledet.bj.bcebos.com/models/ppyolov2_r101vd_dcn_365e_coco.pdparams'
 }
 
 
---
+++ tutorials/train/detection/ppyolov2.py
@@ -0,0 +1,54 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+import paddlex as pdx
+from paddlex import transforms
+import cv2
+
+dataset = 'https://bj.bcebos.com/paddlex/datasets/insect_det.tar.gz'
+pdx.utils.download_and_decompress(dataset, path='./')
+
+train_transforms = transforms.Compose([
+    transforms.MixupImage(mixup_epoch=250), transforms.RandomDistort(),
+    transforms.RandomExpand(im_padding_value=[123.675, 116.28, 103.53]),
+    transforms.RandomCrop(), transforms.RandomHorizontalFlip(),
+    transforms.BatchRandomResize(
+        target_size=[
+            320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672, 704,
+            736, 768
+        ],
+        interp='RANDOM'), transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+eval_transforms = transforms.Compose([
+    transforms.Resize(
+        640, 640, interp=cv2.INTER_CUBIC), transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_dataset = pdx.datasets.VOCDetection(
+    data_dir='insect_det',
+    file_list='insect_det/train_list.txt',
+    label_list='insect_det/labels.txt',
+    transforms=train_transforms,
+    shuffle=True)
+
+eval_dataset = pdx.datasets.VOCDetection(
+    data_dir='insect_det',
+    file_list='insect_det/val_list.txt',
+    label_list='insect_det/labels.txt',
+    transforms=eval_transforms,
+    shuffle=False)
+
+num_classes = len(train_dataset.labels)
+model = pdx.det.PPYOLO(num_classes=num_classes, backbone='ResNet50_vd_dcn')
+
+model.train(
+    num_epochs=270,
+    train_dataset=train_dataset,
+    train_batch_size=8,
+    eval_dataset=eval_dataset,
+    learning_rate=0.000125,
+    lr_decay_epochs=[210, 240],
+    save_dir='output/ppyolov2_r50vd_dcn')
