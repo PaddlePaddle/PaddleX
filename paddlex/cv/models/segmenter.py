16984f4258f88c3a23f94d45ee6e7114a26c54e5
will-jl944 <lujiafeng@baidu.com>
2021-05-17 16:58:24 +0800

    fix the bug that to_rgb is done twice in preprocess of segmenter

--- paddlex/cv/models/segmenter.py
+++ paddlex/cv/models/segmenter.py
@@ -301,7 +301,7 @@ class BaseSegmenter(BaseModel):
         for im in images:
             sample = {'image': im}
             if isinstance(sample['image'], str):
-                sample = Decode()(sample)
+                sample = Decode(to_rgb=False)(sample)
             ori_shape = sample['image'].shape[:2]
             im = transforms(sample)[0]
             batch_im.append(im)
