eb9ee0c51ea756eea6aa88a43c5a4ac3c06cc3c3
FlyingQianMM <245467267@qq.com>
2021-05-26 06:54:16 +0000

    set gui_mode to true

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = "PaddlePaddle Entire Process Development Toolkit"
 
 setuptools.setup(
     name="paddlex",
-    version='1.3.10',
+    version='1.3.11',
     author="paddlex",
     author_email="paddlex@baidu.com",
     description=long_description,
