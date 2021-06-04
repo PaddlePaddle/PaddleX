9f14f10006ef88a0f6f7904b505d50289a81b41c
will-jl944 <lujiafeng@baidu.com>
2021-06-04 19:25:57 +0800

    add decompress to paddlex.utils

--- dygraph/paddlex/utils/__init__.py
+++ dygraph/paddlex/utils/__init__.py
@@ -19,6 +19,6 @@ from .utils import (seconds_to_hms, get_encoding, get_single_card_bs, dict2str,
                     DisablePrint)
 from .checkpoint import get_pretrain_weights, load_pretrain_weights
 from .env import get_environ_info, get_num_workers, init_parallel_env
-from .download import download_and_decompress
+from .download import download_and_decompress, decompress
 from .stats import SmoothedValue, TrainingStats
 from .shm import _get_shared_memory_size_in_M
