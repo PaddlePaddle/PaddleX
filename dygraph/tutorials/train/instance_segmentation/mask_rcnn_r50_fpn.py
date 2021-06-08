18d93b159761cb22574c436b1357023b0708b615
will-jl944 <lujiafeng@baidu.com>
2021-06-08 21:31:31 +0800

    add maskrcnn tutorial

--- dygraph/tutorials/train/instance_segmentation/mask_rcnn_r50_fpn.py
+++ dygraph/tutorials/train/instance_segmentation/mask_rcnn_r50_fpn.py
@@ -8,51 +8,47 @@ pdx.utils.download_and_decompress(dataset, path='./')
 # 定义训练和验证时的transforms
 # API说明：https://github.com/PaddlePaddle/PaddleX/blob/release/2.0-rc/paddlex/cv/transforms/operators.py
 train_transforms = T.Compose([
-    T.MixupImage(mixup_epoch=250), T.RandomDistort(),
-    T.RandomExpand(im_padding_value=[123.675, 116.28, 103.53]), T.RandomCrop(),
-    T.RandomHorizontalFlip(), T.BatchRandomResize(
-        target_sizes=[320, 352, 384, 416, 448, 480, 512, 544, 576, 608],
-        interp='RANDOM'), T.Normalize(
+    T.RandomResizeByShort(
+        short_sizes=[640, 672, 704, 736, 768, 800],
+        max_size=1333,
+        interp='CUBIC'), T.RandomHorizontalFlip(), T.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 eval_transforms = T.Compose([
-    T.Resize(
-        608, interp='CUBIC'), T.Normalize(
+    T.ResizeByShort(
+        short_size=800, max_size=1333, interp='CUBIC'), T.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 # 定义训练和验证所用的数据集
-# API说明：https://github.com/PaddlePaddle/PaddleX/blob/release/2.0-rc/paddlex/cv/datasets/voc.py#L29
-train_dataset = pdx.datasets.VOCDetection(
-    data_dir='insect_det',
-    file_list='insect_det/train_list.txt',
-    label_list='insect_det/labels.txt',
+# API说明：https://github.com/PaddlePaddle/PaddleX/blob/develop/dygraph/paddlex/cv/datasets/coco.py#L26
+train_dataset = pdx.datasets.CocoDetection(
+    data_dir='xiaoduxiong_ins_det/JPEGImages',
+    ann_file='xiaoduxiong_ins_det/train.json',
     transforms=train_transforms,
     shuffle=True)
-
-eval_dataset = pdx.datasets.VOCDetection(
-    data_dir='insect_det',
-    file_list='insect_det/val_list.txt',
-    label_list='insect_det/labels.txt',
-    transforms=eval_transforms,
-    shuffle=False)
+eval_dataset = pdx.datasets.CocoDetection(
+    data_dir='xiaoduxiong_ins_det/JPEGImages',
+    ann_file='xiaoduxiong_ins_det/val.json',
+    transforms=eval_transforms)
 
 # 初始化模型，并进行训练
 # 可使用VisualDL查看训练指标，参考https://github.com/PaddlePaddle/PaddleX/tree/release/2.0-rc/tutorials/train#visualdl可视化训练指标
 num_classes = len(train_dataset.labels)
-model = pdx.models.YOLOv3(num_classes=num_classes, backbone='DarkNet53')
+model = pdx.models.MaskRCNN(
+    num_classes=num_classes, backbone='ResNet50', with_fpn=True)
 
 # API说明：https://github.com/PaddlePaddle/PaddleX/blob/release/2.0-rc/paddlex/cv/models/detector.py#L155
 # 各参数介绍与调整说明：https://paddlex.readthedocs.io/zh_CN/develop/appendix/parameters.html
 model.train(
-    num_epochs=270,
+    num_epochs=12,
     train_dataset=train_dataset,
-    train_batch_size=8,
+    train_batch_size=1,
     eval_dataset=eval_dataset,
-    learning_rate=0.001 / 8,
-    warmup_steps=1000,
+    learning_rate=0.00125,
+    lr_decay_epochs=[8, 11],
+    warmup_steps=10,
     warmup_start_lr=0.0,
-    save_interval_epochs=5,
-    lr_decay_epochs=[216, 243],
-    save_dir='output/yolov3_darknet53')
+    save_dir='output/mask_rcnn_r50_fpn',
+    use_vdl=True)
