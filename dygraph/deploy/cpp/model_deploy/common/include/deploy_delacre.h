7be40a9c85c5fe2ab9d08e250919364ad2dc84d1
heliqi <1101791222@qq.com>
2021-06-29 18:04:19 +0800

    build lib

--- dygraph/deploy/cpp/model_deploy/common/include/deploy_delacre.h
+++ dygraph/deploy/cpp/model_deploy/common/include/deploy_delacre.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,19 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #pragma once
 
-#include <memory>
-#include <string>
-#include <iostream>
-
-#include "model_deploy/common/include/deploy_delacre.h"
-#include "model_deploy/common/include/output_struct.h"
-#include "model_deploy/common/include/model_factory.h"
-#include "model_deploy/engine/include/engine.h"
-
-namespace PaddleDeploy {
-
-PD_INFER_DECL Model* CreateModel(const std::string& name);
-
-}  // namespace PaddleDeploy
+#if defined(_WIN32)
+#ifdef PADDLEX_DEPLOY
+#define PD_INFER_DECL __declspec(dllexport)
+#else
+#define PD_INFER_DECL __declspec(dllimport)
+#endif  // PADDLEX_DEPLOY
+#else
+#define PD_INFER_DECL
+#endif  // _WIN32
