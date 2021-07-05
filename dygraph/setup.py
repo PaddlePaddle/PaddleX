196abd677b4268548a406e4a95821a2cf4e44d7d
FlyingQianMM <245467267@qq.com>
2021-07-05 13:49:42 +0000

    set version to 2.0.0rc3

--- dygraph/setup.py
+++ dygraph/setup.py
@@ -19,7 +19,7 @@ long_description = "PaddlePaddle Entire Process Development Toolkit"
 
 setuptools.setup(
     name="paddlex",
-    version='2.0.0rc2',
+    version='2.0.0rc3',
     author="paddlex",
     author_email="paddlex@baidu.com",
     description=long_description,
