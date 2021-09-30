73658a64419235f75d0762a7d5538cd6250cdc04
will-jl944 <lujiafeng@baidu.com>
2021-09-30 11:36:01 +0800

    update year in copyright

--- paddlex_restful/restful/project/evaluate/draw_pred_result.py
+++ paddlex_restful/restful/project/evaluate/draw_pred_result.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved
+# copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
