abb74310288b75f2493afbae268918a7ab60431d
cuicheng01 <cuicheng_smile@163.com>
2024-06-24 12:45:33 +0000

    fix copyright

--- paddlex/repo_apis/PaddleClas_api/cls/config.py
+++ paddlex/repo_apis/PaddleClas_api/cls/config.py
@@ -1,13 +1,18 @@
-# !/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-################################################################################
+# copyright (c) 2024 PaddlePaddle Authors. All Rights Reserve.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Copyright (c) 2024 Baidu.com, Inc. All Rights Reserved
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-################################################################################
-"""
-Author: PaddlePaddle Authors
-"""
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import yaml
 from typing import Union
 from paddleclas.ppcls.utils.config import get_config, override_config
