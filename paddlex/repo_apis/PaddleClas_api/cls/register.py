0d8bb154ff8f55f9894ece5807fc0af8e9609cc9
Zheng-Bicheng <zheng_bicheng@outlook.com>
2024-09-20 13:15:43 +0800

    update LZ-MobileNetV3_large_x1_0

--- paddlex/repo_apis/PaddleClas_api/cls/register.py
+++ paddlex/repo_apis/PaddleClas_api/cls/register.py
@@ -543,15 +543,6 @@ register_model_info(
 
 register_model_info(
     {
-        "model_name": "LZ-MobileNetV3_small_x1_0",
-        "suite": "Cls",
-        "config_path": osp.join(PDX_CONFIG_DIR, "LZ-MobileNetV3_small_x1_0.yaml"),
-        "supported_apis": ["train", "evaluate", "predict", "export"],
-    }
-)
-
-register_model_info(
-    {
         "model_name": "MobileNetV3_small_x0_35",
         "suite": "Cls",
         "config_path": osp.join(PDX_CONFIG_DIR, "MobileNetV3_small_x0_35.yaml"),
@@ -582,6 +573,15 @@ register_model_info(
         "model_name": "MobileNetV3_small_x1_0",
         "suite": "Cls",
         "config_path": osp.join(PDX_CONFIG_DIR, "MobileNetV3_small_x1_0.yaml"),
+        "supported_apis": ["train", "evaluate", "predict", "export"],
+    }
+)
+
+register_model_info(
+    {
+        "model_name": "LZ-MobileNetV3_large_x1_0",
+        "suite": "Cls",
+        "config_path": osp.join(PDX_CONFIG_DIR, "LZ-MobileNetV3_large_x1_0.yaml"),
         "supported_apis": ["train", "evaluate", "predict", "export"],
     }
 )
