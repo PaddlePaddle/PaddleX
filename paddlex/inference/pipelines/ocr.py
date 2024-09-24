b26ce06df09eff70370579777b35f810fff92606
gaotingquan <gaotingquan@baidu.com>
2024-09-24 14:01:49 +0000

    bugfix

--- paddlex/inference/pipelines/ocr.py
+++ paddlex/inference/pipelines/ocr.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .base import BasePipeline
-from ..components import CropByPolys
+from ..components import SortBoxes, CropByPolys
 from ..results import OCRResult
+from .base import BasePipeline
 
 
 class OCRPipeline(BasePipeline):
@@ -23,21 +23,25 @@ class OCRPipeline(BasePipeline):
     entities = "ocr"
 
     def __init__(
-        self, det_model, rec_model, rec_batch_size, predictor_kwargs=None, is_curve=False, **kwargs
+        self,
+        det_model,
+        rec_model,
+        rec_batch_size,
+        predictor_kwargs=None,
     ):
         super().__init__(predictor_kwargs)
         self._det_predict = self._create_predictor(det_model)
         self._rec_predict = self._create_predictor(rec_model, batch_size=rec_batch_size)
-        # TODO: foo
-        if is_curve:
-            det_box_type = 'poly'
-        else:
-            det_box_type = 'quad'
-        self._crop_by_polys = CropByPolys(det_box_type=det_box_type)
+        is_curve = self._det_predict.model_name in [
+            "PP-OCRv4_mobile_seal_det",
+            "PP-OCRv4_server_seal_det",
+        ]
+        self._sort_boxes = SortBoxes()
+        self._crop_by_polys = CropByPolys(det_box_type="poly" if is_curve else "quad")
 
     def predict(self, x):
         for det_res in self._det_predict(x):
-            single_img_res = det_res
+            single_img_res = next(self._sort_boxes(det_res))
             single_img_res["rec_text"] = []
             single_img_res["rec_score"] = []
             if len(single_img_res["dt_polys"]) > 0:
@@ -45,4 +49,4 @@ class OCRPipeline(BasePipeline):
                 for rec_res in self._rec_predict(all_subs_of_img):
                     single_img_res["rec_text"].append(rec_res["rec_text"])
                     single_img_res["rec_score"].append(rec_res["rec_score"])
-            yield single_img_res
+            yield OCRResult(single_img_res)
