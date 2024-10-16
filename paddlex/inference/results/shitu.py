940502d9d04394740f3f7659b9012bda80226797
gaotingquan <gaotingquan@baidu.com>
2024-10-16 08:28:50 +0000

    support PP-ShiTu

--- paddlex/inference/results/shitu.py
+++ paddlex/inference/results/shitu.py
@@ -12,7 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .transforms import *
-from .paddle_predictor import *
-from .task_related import *
-from .retrieval import *
+import numpy as np
+from .base import CVResult
+from .det import draw_box
+
+
+class ShiTuResult(CVResult):
+
+    def _to_img(self):
+        """apply"""
+        image = self._img_reader.read(self["input_path"])
+        boxes = [
+            {
+                "coordinate": box["coordinate"],
+                "label": box["labels"][0],
+                "score": box["rec_scores"][0],
+            }
+            for box in self["boxes"]
+        ]
+        image = draw_box(image, boxes)
+        return image
