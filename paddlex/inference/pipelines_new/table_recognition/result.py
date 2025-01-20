bd51f6b5fa02d3265cdcdd6db94f655e37224b53
Liu Jiaxuan <85537209+liu-jiaxuan@users.noreply.github.com>
2025-01-20 22:13:52 +0800

    Update result.py

--- paddlex/inference/pipelines_new/table_recognition/result.py
+++ paddlex/inference/pipelines_new/table_recognition/result.py
@@ -102,14 +102,14 @@ class TableRecognitionResult(BaseCVResult, HtmlMixin, XlsxMixin):
 
         if len(self["table_res_list"]) > 0:
             table_cell_img = Image.fromarray(copy.deepcopy(self["doc_preprocessor_res"]["output_img"]))
-            draw = ImageDraw.Draw(table_cell_img)
-            rectangle_color = (255, 0, 0)  # 红色
+            table_draw = ImageDraw.Draw(table_cell_img)
+            rectangle_color = (255, 0, 0)
             for sno in range(len(self["table_res_list"])):
                 table_res = self["table_res_list"][sno]
                 cell_box_list = table_res["cell_box_list"]
                 for box in cell_box_list:
                     x1, y1, x2, y2 = [int(pos) for pos in box]
-                    draw.rectangle([x1, y1, x2, y2], outline=rectangle_color, width=2)
+                    table_draw.rectangle([x1, y1, x2, y2], outline=rectangle_color, width=2)
             res_img_dict["table_cell_img"] = table_cell_img
         return res_img_dict
 
