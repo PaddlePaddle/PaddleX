bb6f4ae6b82818a8558a7eb252979e02904a773a
Bobholamovic <bob1998425@hotmail.com>
2025-01-23 16:10:43 +0800

    Add table_recognition_v2

--- paddlex/inference/serving/basic_serving/_pipeline_apps/pp_chatocrv4_doc.py
+++ paddlex/inference/serving/basic_serving/_pipeline_apps/pp_chatocrv4_doc.py
@@ -19,7 +19,7 @@ from fastapi import FastAPI
 from ...infra import utils as serving_utils
 from ...infra.config import AppConfig
 from ...infra.models import ResultResponse
-from ...schemas import pp_chatocrv3_doc as schema
+from ...schemas import pp_chatocrv4_doc as schema
 from .._app import create_app, primary_operation
 from ._common import common
 from ._common import ocr as ocr_common
