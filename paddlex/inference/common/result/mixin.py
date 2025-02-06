1e9bf9ddd6fd69a2f1f27fe9510b5840231b8052
cuicheng01 <cuicheng_smile@163.com>
2025-02-06 06:57:07 +0000

    fix & polish layout_parsing_v2

--- paddlex/inference/common/result/mixin.py
+++ paddlex/inference/common/result/mixin.py
@@ -15,6 +15,7 @@
 from typing import Union, Tuple, List, Dict, Any, Iterator
 from abc import abstractmethod
 from pathlib import Path
+import os
 import mimetypes
 import json
 import copy
@@ -379,7 +380,6 @@ class CSVMixin:
 
         if not _is_csv_file(save_path):
             fn = Path(self._get_input_fn())
-            fn = Path(self._get_input_fn())
             stem = fn.stem
             base_save_path = Path(save_path)
             for key in self.csv:
@@ -597,11 +597,18 @@ class VideoMixin:
 
 
 class MarkdownMixin:
+    """Mixin class for adding Markdown handling capabilities."""
 
     def __init__(self, *args: list, **kwargs: dict):
+        """Initializes the Markdown writer and appends the save_to_markdown method to the save functions.
+
+        Args:
+            *args: Positional arguments to be passed to the MarkdownWriter constructor.
+            **kwargs: Keyword arguments to be passed to the MarkdownWriter constructor.
+        """
         self._markdown_writer = MarkdownWriter(*args, **kwargs)
+        self._img_writer = ImageWriter(*args, **kwargs)
         self._save_funcs.append(self.save_to_markdown)
-        self.save_path = None
 
     @abstractmethod
     def _to_markdown(self):
@@ -617,33 +624,43 @@ class MarkdownMixin:
         return self._to_markdown()
 
     def save_to_markdown(self, save_path, *args, **kwargs):
-        save_path = Path(save_path)
-        if not save_path.suffix.lower() == ".md":
-            save_path = save_path / f"layout_parsing_result.md"
+        def _is_markdown_file(file_path):
+            markdown_extensions = {".md", ".markdown", ".mdown", ".mkd"}
+            _, ext = os.path.splitext(file_path)
+            if ext.lower() in markdown_extensions:
+                return True
+            mime_type, _ = mimetypes.guess_type(file_path)
+            return mime_type == "text/markdown"
 
-        self.save_path = save_path
-
-        self._save_list_data(
+        if not _is_markdown_file(save_path):
+            fn = Path(self._get_input_fn())
+            suffix = fn.suffix if _is_markdown_file(fn) else ".md"
+            stem = fn.stem
+            base_save_path = Path(save_path)
+            save_path = base_save_path / f"{stem}{suffix}"
+            self.save_path = save_path
+        else:
+            self.save_path = save_path
+        self._save_data(
             self._markdown_writer.write,
-            save_path,
+            self._img_writer.write,
+            self.save_path,
             self.markdown,
             *args,
             **kwargs,
         )
 
-    def _save_list_data(self, save_func, save_path, data, *args, **kwargs):
+    def _save_data(
+        self, save_mkd_func, save_img_func, save_path, data, *args, **kwargs
+    ):
         save_path = Path(save_path)
         if data is None:
             return
-        if isinstance(data, list):
-            for idx, single in enumerate(data):
-                save_func(
-                    (
-                        save_path.parent / f"{save_path.stem}_{idx}{save_path.suffix}"
-                    ).as_posix(),
-                    single,
-                    *args,
-                    **kwargs,
-                )
-        save_func(save_path.as_posix(), data, *args, **kwargs)
-        logging.info(f"The result has been saved in {save_path}.")
+        for key, value in data.items():
+            if isinstance(value, str):
+                save_mkd_func(save_path.as_posix(), value, *args, **kwargs)
+            if isinstance(value, dict):
+                base_save_path = save_path.parent
+                for img_path, img_data in value.items():
+                    save_path = base_save_path / img_path
+                    save_img_func(save_path.as_posix(), img_data, *args, **kwargs)
