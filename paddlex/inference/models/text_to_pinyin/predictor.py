b2213d1334d383cab11086016740e32197777474
zxcd <228587199@qq.com>
2025-03-03 08:18:19 +0000

    add text to pinyin

---
+++ paddlex/inference/models/text_to_pinyin/predictor.py
@@ -0,0 +1,89 @@
+# copyright (c) 2024 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import lazy_paddle as paddle
+import numpy as np
+
+from ....utils.func_register import FuncRegister
+from ...common.batch_sampler import TextBatchSampler
+
+from ..base import BasicPredictor
+from .result import TextToPinyinResult
+from ....modules.text_to_pinyin.model_list import MODELS
+
+
+class TextToPinyinPredictor(BasicPredictor):
+
+    entities = MODELS
+
+    def __init__(self, *args, **kwargs):
+        """Initializes TextSegmentPredictor.
+
+        Args:
+            *args: Arbitrary positional arguments passed to the superclass.
+            **kwargs: Arbitrary keyword arguments passed to the superclass.
+        """
+        super().__init__(*args, **kwargs)
+        self.model = self._build()
+
+    def _build_batch_sampler(self):
+        """Builds and returns an TextBatchSampler instance.
+
+        Returns:
+            TextBatchSampler: An instance of TextBatchSampler.
+        """
+        return TextBatchSampler()
+
+    def _get_result_class(self):
+        """Returns the result class, TextToPinyinResult.
+
+        Returns:
+            type: The TextToPinyinResult class.
+        """
+        return TextToPinyinResult
+
+    def _build(self):
+        """Build the model, audio reader based on the configuration.
+
+        Returns:
+            AudioReader: An instance of AudioReader.
+        """
+        from .processors import (
+            G2PWOnnxConverter,
+        )
+
+        # build model
+        model = G2PWOnnxConverter(
+            model_dir=self.model_dir, style="pinyin", enable_non_tradional_chinese=True
+        )
+        return model
+
+    def process(self, batch_data):
+        """
+        Process a batch of data through the preprocessing, inference, and postprocessing.
+
+        Args:
+            batch_data (List[Union[str, np.ndarray], ...]): A batch of input data (e.g., audio file paths).
+
+        Returns:
+            dict: A dictionary containing the input path and result. The result include 'text', 'segments' and 'language'.
+        """
+
+        # load mel_filters from resource_dir and extract feature for audio
+        result = self.model(batch_data[0])[0]
+
+        return {
+            "input_path": batch_data,
+            "result": [result],
+        }
