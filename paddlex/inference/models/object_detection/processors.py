3ed228ea8a8f25a843c446b9bb8a06942cce2b2b
zhangyue66 <zhangyue66@baidu.com>
2025-03-14 19:12:14 +0800

    update

--- paddlex/inference/models/object_detection/processors.py
+++ paddlex/inference/models/object_detection/processors.py
@@ -27,7 +27,7 @@ Boxes = List[dict]
 Number = Union[int, float]
 
 
-@benchmark.timeit_with_options(name=None, is_read_operation=True)
+@benchmark.timeit
 class ReadImage(CommonReadImage):
     """Reads images from a list of raw image data or file paths."""
 
