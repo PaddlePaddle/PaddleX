db41be58fd7d2088a2c573fe5e70986417991401
Bobholamovic <bob1998425@hotmail.com>
2025-03-21 20:11:00 +0800

    Improve

--- .precommit/check_custom.py
+++ .precommit/check_custom.py
@@ -16,20 +16,23 @@ import os
 import re
 import sys
 
-LICENSE_TEXT = r"""# Copyright \(c\) (?:20\d\d) PaddlePaddle Authors\. All Rights Reserved\.
+YEAR_PATTERN = r"(?:20\d\d)"
+LICENSE_TEXT = re.escape(
+    """# Copyright (c) <YEAR_PATTERN> PaddlePaddle Authors. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2\.0 \(the "License"\);
-# you may not use this file except in compliance with the License\.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2\.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License\.
+# limitations under the License.
 """
+).replace("<YEAR_PATTERN>", YEAR_PATTERN)
 
 
 def check(file_path):
