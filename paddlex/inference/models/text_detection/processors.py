dfcdeb1211a7c2b3ffdc0b313c505e81375df431
zhangyue66 <zhangyue66@baidu.com>
2025-03-24 22:09:44 +0800

    opt Normalize op

--- paddlex/inference/models/text_detection/processors.py
+++ paddlex/inference/models/text_detection/processors.py
@@ -191,19 +191,29 @@ class NormalizeImage:
         super().__init__()
         if isinstance(scale, str):
             scale = eval(scale)
+        self.order = order
         self.scale = np.float32(scale if scale is not None else 1.0 / 255.0)
         mean = mean if mean is not None else [0.485, 0.456, 0.406]
         std = std if std is not None else [0.229, 0.224, 0.225]
 
-        shape = (3, 1, 1) if order == "chw" else (1, 1, 3)
-        self.mean = np.array(mean).reshape(shape).astype("float32")
-        self.std = np.array(std).reshape(shape).astype("float32")
+        self.alpha = [self.scale / std[i] for i in range(len(std))]
+        self.beta = [-mean[i] / std[i] for i in range(len(std))]
 
     def __call__(self, imgs):
         """apply"""
 
         def norm(img):
-            return (img.astype("float32") * self.scale - self.mean) / self.std
+            if self.order == "chw":
+                img = np.transpose(img, (2, 0, 1))
+
+            split_im = list(cv2.split(img))
+            for c in range(img.shape[2]):
+                split_im[c] = (
+                    split_im[c].astype(np.float32) * self.alpha[c] + self.beta[c]
+                )
+
+            res = cv2.merge(split_im)
+            return res
 
         return [norm(img) for img in imgs]
 
