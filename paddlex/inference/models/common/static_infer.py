ef28bb7f8f90aeb7b3eb4e50ee20ee3f01d99b04
gaotingquan <gaotingquan@baidu.com>
2025-04-16 09:07:32 +0000

    support enable_new_ir when using pdmodel on mlu or npu device

--- paddlex/inference/models/common/static_infer.py
+++ paddlex/inference/models/common/static_infer.py
@@ -409,6 +409,8 @@ class PaddleInfer(StaticInfer):
                 config.set_optimization_level(3)
             elif self._option.device_type == "npu":
                 config.enable_custom_device("npu", self._option.device_id)
+                if hasattr(config, "enable_new_ir"):
+                    config.enable_new_ir(self._option.enable_new_ir)
                 if hasattr(config, "enable_new_executor"):
                     config.enable_new_executor()
             elif self._option.device_type == "xpu":
@@ -418,6 +420,8 @@ class PaddleInfer(StaticInfer):
                     config.enable_new_executor()
             elif self._option.device_type == "mlu":
                 config.enable_custom_device("mlu")
+                if hasattr(config, "enable_new_ir"):
+                    config.enable_new_ir(self._option.enable_new_ir)
                 if hasattr(config, "enable_new_executor"):
                     config.enable_new_executor()
             elif self._option.device_type == "gcu":
@@ -425,8 +429,9 @@ class PaddleInfer(StaticInfer):
 
                 gcu_passes.setUp()
                 config.enable_custom_device("gcu")
-                if hasattr(config, "enable_new_executor"):
+                if hasattr(config, "enable_new_ir"):
                     config.enable_new_ir()
+                if hasattr(config, "enable_new_executor"):
                     config.enable_new_executor()
                 else:
                     pass_builder = config.pass_builder()
