578e415e86aa661ac39e5bdb4add94293add1e42
a31413510 <31413510@qq.com>
2025-04-23 18:51:19 +0800

    update npu pipeline list

--- paddlex/inference/models/common/static_infer.py
+++ paddlex/inference/models/common/static_infer.py
@@ -373,7 +373,7 @@ class PaddleInfer(StaticInfer):
             logging.debug("`device_id` has been set to None")
 
         if (
-            self._option.device_type in ("gpu", "dcu")
+            self._option.device_type in ("gpu", "dcu", "npu")
             and self._option.device_id is None
         ):
             self._option.device_id = 0
