fd354be87f6aa126d45efbf20ceec00857df17ee
zhangjinghong <zhangjinghong@baidu.com>
2025-06-26 08:24:30 +0000

    add tts models

---
+++ paddlex/inference/models/text_to_speech_vocoder/predictor.py
@@ -0,0 +1,90 @@
+# copyright (c) 2025 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+from ....utils.func_register import FuncRegister
+from ...common.batch_sampler import AudioBatchSampler
+
+from ..base import BasePredictor
+from .result import PwganResult
+from ....modules.text_to_speech_vocoder.model_list import MODELS
+
+
+class PwganPredictor(BasePredictor):
+
+    entities = MODELS
+
+    def __init__(self, *args, **kwargs):
+        """Initializes FastspeechPredictor.
+
+        Args:
+            *args: Arbitrary positional arguments passed to the superclass.
+            **kwargs: Arbitrary keyword arguments passed to the superclass.
+        """
+        super().__init__(*args, **kwargs)
+        self.model = self._build()
+        self.model_dir = "/home/zhangjinghong/PaddleSpeech/examples/csmsc/tts3/fastspeech2_nosil_baker_ckpt_0.4/inference"
+    def _build_batch_sampler(self):
+        """Builds and returns an AudioBatchSampler instance.
+
+        Returns:
+            AudioBatchSampler: An instance of AudioBatchSampler.
+        """
+        return AudioBatchSampler()
+
+    def _get_result_class(self):
+        """Returns the result class, PwganResult.
+
+        Returns:
+            type: The PwganResult class.
+        """
+        return PwganResult
+
+    def _build(self):
+        """Build the model.
+
+        Returns:
+            Pwgan: An instance of Pwgan.
+        """
+        from .processors import get_predictor
+        model = get_predictor(
+            model_dir=str(self.model_dir),
+            model_file=self.config['Global']['model'] + ".pdmodel",
+            params_file=self.config['Global']['model'] + ".pdiparams",
+            device=self.config['Global']['device'],
+            use_trt=self.config['Global']['use_trt'],
+            use_mkldnn=self.config['Global']['use_mkldnn'],
+            cpu_threads=self.config['Global']['cpu_threads'],
+            precision=self.config['Global']['precision'],)
+        return model
+
+    def process(self, batch_data):
+        """
+        Process a batch of data through the preprocessing, inference, and postprocessing.
+
+        Args:
+            batch_data (List[Union[str], ...]): A batch of input phone data.
+
+        Returns:
+            dict: A dictionary containing the input path and result. The result include the output pinyin dict.
+        """
+        from .processors import get_voc_output
+        mel = batch_data[0]
+        wav = get_voc_output(voc_predictor=self.model, input=mel)
+        result = np.array(wav).reshape(1,-1)
+        return {
+            "result": result,
+        }
+
---
+++ paddlex/inference/models/text_to_speech_vocoder/processors.py
@@ -0,0 +1,135 @@
+# copyright (c) 2025 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
+
+import os
+import lazy_paddle as paddle
+# import paddle
+
+def get_predictor(
+        model_dir: Optional[os.PathLike]=None,
+        model_file: Optional[os.PathLike]=None,
+        params_file: Optional[os.PathLike]=None,
+        device: str='cpu',
+        # for gpu
+        use_trt: bool=False,
+        device_id: int=0,
+        # for trt
+        use_dynamic_shape: bool=True,
+        min_subgraph_size: int=5,
+        # for cpu
+        cpu_threads: int=1,
+        use_mkldnn: bool=False,
+        # for trt or mkldnn
+        precision: int="fp32"):
+    """
+    Args:
+        model_dir (os.PathLike): root path of model.pdmodel and model.pdiparams.
+        model_file (os.PathLike): name of model_file.
+        params_file (os.PathLike): name of params_file.
+        device (str): Choose the device you want to run, it can be: cpu/gpu, default is cpu.
+        use_trt (bool): whether to use TensorRT or not in GPU.
+        device_id (int): Choose your device id, only valid when the device is gpu, default 0.
+        use_dynamic_shape (bool): use dynamic shape or not in TensorRT.
+        use_mkldnn (bool): whether to use MKLDNN or not in CPU.
+        cpu_threads (int): num of thread when use CPU.
+        precision (str): mode of running (fp32/fp16/bf16/int8).  
+    """
+    rerun_flag = False
+    if device != "gpu" and use_trt:
+        raise ValueError(
+            "Predict by TensorRT mode: {}, expect device=='gpu', but device == {}".
+            format(precision, device))
+    
+    # after paddle 3.0, support new inference interface
+
+    model_name = str(model_file).rsplit('.', 1)[0]
+    assert model_name == str(params_file).split('.')[0], "The prefix of model_file and params_file should be same."
+    config = paddle.inference.Config(model_dir, model_name)
+
+    if paddle.__version__ <= "2.5.2" and paddle.__version__ != "0.0.0":
+        config.enable_memory_optim()
+    config.switch_ir_optim(True)
+    if device == "gpu":
+        config.enable_use_gpu(100, device_id)
+    else:
+        config.disable_gpu()
+        config.set_cpu_math_library_num_threads(cpu_threads)
+        if use_mkldnn:
+            # fp32
+            config.enable_mkldnn()
+            if precision == "int8":
+                config.enable_mkldnn_int8({
+                    "conv2d_transpose", "conv2d", "depthwise_conv2d", "pool2d",
+                    "transpose2", "elementwise_mul"
+                })
+                # config.enable_mkldnn_int8()
+            elif precision in {"fp16", "bf16"}:
+                config.enable_mkldnn_bfloat16()
+            print("MKLDNN with {}".format(precision))
+    if use_trt:
+        if precision == "bf16":
+            print("paddle trt does not support bf16, switching to fp16.")
+            precision = "fp16"
+        precision_map = {
+            "int8": paddle.inference.Config.Precision.Int8,
+            "fp32": paddle.inference.Config.Precision.Float32,
+            "fp16": paddle.inference.Config.Precision.Half,
+        }
+        assert precision in precision_map.keys()
+        pdtxt_name = model_file.split(".")[0] + "_" + precision + ".txt"
+        if use_dynamic_shape:
+            dynamic_shape_file = os.path.join(model_dir, pdtxt_name)
+            if os.path.exists(dynamic_shape_file):
+                config.enable_tuned_tensorrt_dynamic_shape(dynamic_shape_file,
+                                                           True)
+                # for fastspeech2
+                config.exp_disable_tensorrt_ops(["reshape2"])
+                print("trt set dynamic shape done!")
+            else:
+                # In order to avoid memory overflow when collecting dynamic shapes, it is changed to use CPU.
+                config.disable_gpu()
+                config.set_cpu_math_library_num_threads(10)
+                config.collect_shape_range_info(dynamic_shape_file)
+                print("Start collect dynamic shape...")
+                rerun_flag = True
+
+        if not rerun_flag:
+            print("Tensor RT with {}".format(precision))
+            config.enable_tensorrt_engine(
+                workspace_size=1 << 30,
+                max_batch_size=1,
+                min_subgraph_size=min_subgraph_size,
+                precision_mode=precision_map[precision],
+                use_static=True,
+                use_calib_mode=False, )
+
+    predictor = paddle.inference.create_predictor(config)
+    return predictor
+def get_voc_output(voc_predictor, input):
+    voc_input_names = voc_predictor.get_input_names()
+    mel_handle = voc_predictor.get_input_handle(voc_input_names[0])
+    mel_handle.reshape(input.shape)
+    mel_handle.copy_from_cpu(input)
+
+    voc_predictor.run()
+    voc_output_names = voc_predictor.get_output_names()
+    voc_output_handle = voc_predictor.get_output_handle(voc_output_names[0])
+    wav = voc_output_handle.copy_to_cpu()
+    return wav
