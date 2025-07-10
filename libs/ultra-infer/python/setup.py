cf3b4cdcbcac264744063954e68ce83663373e42
zhouxin <zhou.xin@mail.ustc.edu.cn>
2025-07-10 09:38:48 +0800

    Update pandas

--- libs/ultra-infer/python/setup.py
+++ libs/ultra-infer/python/setup.py
@@ -436,7 +436,7 @@ if sys.version_info[0] == 3:
 
 extras_require["pyonly"] = [
     "pillow<10.0.0",
-    "pandas>=0.25.0,<=1.3.5",
+    "pandas>=0.25.0,<=1.4.5",
     "pycocotools",
     "matplotlib",
     "chinese_calendar",
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ DEP_SPECS = {
     "opencv-contrib-python": "== 4.10.0.84",
     "openpyxl": "",
     "packaging": "",
-    "pandas": ">= 1.3",
+    "pandas": ">= 1.4.5",
     "pillow": "",
     "premailer": "",
     "prettytable": "",
