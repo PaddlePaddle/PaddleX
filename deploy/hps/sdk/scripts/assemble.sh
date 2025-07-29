7c2a7eae0d241b50e2aa0218dc3bf2d91b9e55ed
baijiahao01 <782537644@qq.com>
2025-07-29 14:23:27 +0800

    fix assemble.sh docker image format

--- deploy/hps/sdk/scripts/assemble.sh
+++ deploy/hps/sdk/scripts/assemble.sh
@@ -8,5 +8,5 @@ docker run \
     -v "$(pwd)":/workspace \
     -w /workspace \
     --rm \
-    python3.10:sha256:875c3591e586f66aa65621926230925144920c951902a6c2eef005d9783a7ca7 \
+    python:3.10@sha256:6ff000548a4fa34c1be02624836e75e212d4ead8227b4d4381c3ae998933a922 \
     /bin/bash scripts/_assemble.sh "$@"
