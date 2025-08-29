956d0cfa069db558656b4841bf3ee7af6df8e49a
Bobholamovic <mhlin425@whu.edu.cn>
2025-08-29 11:15:28 +0000

    Add TODO

--- paddlex/inference/genai/backends/vllm.py
+++ paddlex/inference/genai/backends/vllm.py
@@ -32,6 +32,8 @@ def register_models():
 
 
 def run_vllm_server(host, port, model_name, model_dir, config, chat_template_path):
+    # TODO: Set `max_new_tokens` to align with the local native predictor
+
     require_genai_engine_plugin("vllm-server")
 
     import uvloop
