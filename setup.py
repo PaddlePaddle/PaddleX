2512627792579317582e0ac01b419af216da4fae
zhangyue66 <zhangyue66@baidu.com>
2025-09-29 18:07:52 +0800

    sglang: improve the robustness of SiglipAttention

--- setup.py
+++ setup.py
@@ -205,6 +205,7 @@ EXTRAS = {
             "sglang [all] == 0.4.10.post2",
             "torch == 2.7.1",
             "transformers",
+            "xformers",
         ],
         "genai-vllm-server": [
             "einops",
