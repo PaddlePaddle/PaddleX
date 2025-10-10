8e7d1bd62e27737992ad9878d34744af2fcaa45a
zhangyue66 <zhangyue66@baidu.com>
2025-10-10 16:28:10 +0800

    use flash-attn==2.8.3

--- paddlex/paddlex_cli.py
+++ paddlex/paddlex_cli.py
@@ -369,7 +369,7 @@ def install(args):
         for plugin_type in plugin_types:
             if "vllm" in plugin_type or "sglang" in plugin_type:
                 try:
-                    install_packages(["flash-attn == 2.8.2"], constraints="required")
+                    install_packages(["flash-attn == 2.8.3"], constraints="required")
                 except Exception:
                     logging.error("Installation failed", exc_info=True)
                     sys.exit(1)
