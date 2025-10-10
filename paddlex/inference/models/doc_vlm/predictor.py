ccdd76cd78c2976599fe82746bdce03de7cb80ea
zhangyue66 <zhangyue66@baidu.com>
2025-10-10 14:23:17 +0800

    compatible with RGBA images

--- paddlex/inference/models/doc_vlm/predictor.py
+++ paddlex/inference/models/doc_vlm/predictor.py
@@ -372,6 +372,8 @@ class DocVLMPredictor(BasePredictor):
                     from PIL import Image
 
                     with Image.open(image) as img:
+                        if img.mode == "RGBA":
+                            img = img.convert("RGB")
                         with io.BytesIO() as buf:
                             img.save(buf, format="JPEG")
                             image_url = "data:image/jpeg;base64," + base64.b64encode(
