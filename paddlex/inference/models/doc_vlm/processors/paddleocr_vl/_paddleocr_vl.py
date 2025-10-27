54e39c4745bc87ad5960a060e1b9e831514e0ede
zhang-prog <69562787+zhang-prog@users.noreply.github.com>
2025-10-27 19:21:45 +0800

    update for new chat_template (#4672)
    * update for new chat_template
    
    * adapt to the old chat_template
    
    * fix

--- paddlex/inference/models/doc_vlm/processors/paddleocr_vl/_paddleocr_vl.py
+++ paddlex/inference/models/doc_vlm/processors/paddleocr_vl/_paddleocr_vl.py
@@ -82,7 +82,10 @@ class PaddleOCRVLProcessor(object):
             messages = [
                 {
                     "role": "user",
-                    "content": input_dict["query"],
+                    "content": [
+                        {"type": "image", "image": "placeholder"},  # placeholder
+                        {"type": "text", "text": input_dict["query"]},
+                    ],
                 }
             ]
             prompt = self.tokenizer.apply_chat_template(messages, tokenize=False)
