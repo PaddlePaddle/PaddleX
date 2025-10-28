f7d1e8288be03f2accde7fe5fc0bfe096e6794b9
Bobholamovic <mhlin425@whu.edu.cn>
2025-10-28 14:24:45 +0000

    Support setting paddlex version in dockerfile

--- deploy/genai_vllm_server_docker/build.sh
+++ deploy/genai_vllm_server_docker/build.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 
+paddlex_version='>=3.3.6,<3.4'
+tag_suffix='latest'
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --pdx-version)
+            paddlex_version="==$2"
+            shift
+            shift
+            ;;
+        --tag-suffix)
+            tag_suffix="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
 docker build \
-    -t "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlepaddle/paddlex-genai-vllm-server:${1:latest}" \
+    -t "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlepaddle/paddlex-genai-vllm-server:${tag_suffix}" \
+    --build-arg PADDLEX_VERSION="${paddlex_version}" \
     --build-arg http_proxy="${http_proxy}" \
     --build-arg https_proxy="${https_proxy}" \
     --build-arg no_proxy="${no_proxy}" \
-    --build-arg PIP_INDEX_URL="${PIP_INDEX_URL}" \
     .
