faa5281e90bab9ea5ac80dbae7942080e1287203
Bobholamovic <mhlin425@whu.edu.cn>
2025-10-31 07:14:57 +0000

    Support building docker image for SM120

--- deploy/genai_vllm_server_docker/build.sh
+++ deploy/genai_vllm_server_docker/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 paddlex_version='>=3.3.6,<3.4'
+build_for_sm120='false'
 tag_suffix='latest'
 
 while [[ $# -gt 0 ]]; do
@@ -8,6 +9,10 @@ while [[ $# -gt 0 ]]; do
         --pdx-version)
             paddlex_version="==$2"
             shift
+            shift
+            ;;
+        --sm120)
+            build_for_sm120='true'
             shift
             ;;
         --tag-suffix)
@@ -25,6 +30,7 @@ done
 docker build \
     -t "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlepaddle/paddlex-genai-vllm-server:${tag_suffix}" \
     --build-arg PADDLEX_VERSION="${paddlex_version}" \
+    --build-arg BUILD_FOR_SM120="${build_for_sm120}" \
     --build-arg http_proxy="${http_proxy}" \
     --build-arg https_proxy="${https_proxy}" \
     --build-arg no_proxy="${no_proxy}" \
