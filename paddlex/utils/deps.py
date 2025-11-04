2a13070093529bd90d3233fa1035b5c728bbd3c3
Bobholamovic <mhlin425@whu.edu.cn>
2025-11-04 11:24:52 +0000

    Update FD version and default params

--- paddlex/utils/deps.py
+++ paddlex/utils/deps.py
@@ -306,7 +306,7 @@ def get_genai_fastdeploy_spec(device_type):
     if device_type not in SUPPORTED_DEVICE_TYPES:
         raise ValueError(f"Unsupported device type: {device_type}")
     if device_type == "gpu":
-        return "fastdeploy-gpu == 2.2.1"
+        return "fastdeploy-gpu == 2.2.2"
     else:
         raise AssertionError
 
