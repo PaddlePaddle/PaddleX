e4866f9457b7bbff36be9fc71c9b0812e801aede
Lin Manhui <bob1998425@hotmail.com>
2025-11-18 14:03:54 +0800

    Fix naming error (#4738)

--- paddlex/inference/utils/model_paths.py
+++ paddlex/inference/utils/model_paths.py
@@ -47,8 +47,8 @@ def get_model_paths(
         model_paths["onnx"] = model_dir / f"{model_file_prefix}.onnx"
     if (model_dir / f"{model_file_prefix}.om").exists():
         model_paths["om"] = model_dir / f"{model_file_prefix}.om"
-    if (model_dir / "model.pdparams").exists():
-        model_paths["paddle_dyn"] = model_dir / "model.pdparams"
+    if (model_dir / "model_state.pdparams").exists():
+        model_paths["paddle_dyn"] = model_dir / "model_state.pdparams"
     if (model_dir / "model.safetensors").exists():
         model_paths["safetensors"] = model_dir / "model.safetensors"
     return model_paths
