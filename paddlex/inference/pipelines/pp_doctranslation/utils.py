3752cff59f9f563dce64a3bc395fb2a56a89c64d
SpongeBob0318 <204268140@qq.com>
2025-11-27 04:11:40 +0000

    Merge remote-tracking branch 'upstream/develop' into develop

--- paddlex/inference/pipelines/pp_doctranslation/utils.py
+++ paddlex/inference/pipelines/pp_doctranslation/utils.py
@@ -176,6 +176,7 @@ def translate_html_block(html_block, chunk_size, translate_func, results):
     import copy
 
     from bs4 import BeautifulSoup
+    import copy
 
     # If the HTML is short and simple, translate directly
     if (
