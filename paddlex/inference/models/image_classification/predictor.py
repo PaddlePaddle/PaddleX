77e31e1d53f505046b05be4fb5b34a1142753632
XingweiDeng <aaadddiy@163.com>
2025-12-02 16:12:03 +0800

    Add predictor convert_from_hf

--- paddlex/inference/models/image_classification/predictor.py
+++ paddlex/inference/models/image_classification/predictor.py
@@ -100,7 +100,7 @@ class ClasPredictor(BasePredictor):
             ):
                 with TemporaryDeviceChanger(self.device):
                     infer = PPLCNet.from_pretrained(
-                        self.model_dir, use_safetensors=True
+                        self.model_dir, use_safetensors=True, convert_from_hf=True
                     )
                 infer.eval()
             else:
