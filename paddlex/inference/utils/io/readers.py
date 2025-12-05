329714123cb2037b01fc67e1424a53b7596b28ae
changdazhou <142379845+changdazhou@users.noreply.github.com>
2025-12-05 16:50:06 +0800

    feat: support saving image shape, page_count, and group_id; add ctrl options merge_layout_blocks and markdown_ignore_labels (#4787)
    * feat: support saving image shape, page_count, and group_id; add ctrl options merge_layout_blocks and markdown_ignore_labels
    
    * bugfix for PP-StructureV3 IndexError
    
    * fix handle_funcs_dict in _to_json()

--- paddlex/inference/utils/io/readers.py
+++ paddlex/inference/utils/io/readers.py
@@ -102,7 +102,10 @@ class PDFReader(_BaseReader):
         super().__init__(backend, **bk_args)
 
     def read(self, in_path):
-        yield from self._backend.read_file(str(in_path))
+        yield from self._backend.read_file(in_path)
+
+    def load(self, in_path):
+        return self._backend.load_file(str(in_path))
 
     def _init_backend(self, bk_type, bk_args):
         return PDFReaderBackend(**bk_args)
@@ -291,8 +294,16 @@ class PDFReaderBackend(_BaseReaderBackend):
         self._rotation = rotate
         self._scale = zoom
 
-    def read_file(self, in_path):
+    def load_file(self, in_path):
+        """load pdf file"""
         doc = pdfium.PdfDocument(in_path)
+        return doc
+
+    def read_file(self, in_path):
+        if isinstance(in_path, pdfium.PdfDocument):
+            doc = in_path
+        else:
+            doc = self.load_file(str(in_path))
         try:
             for page in doc:
                 yield page.render(scale=self._scale, rotation=self._rotation).to_numpy()
