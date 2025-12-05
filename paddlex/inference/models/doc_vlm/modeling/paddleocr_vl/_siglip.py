2ca278fb0ddaa818edaf94e2fc05666d79e36562
tianyuzhou668 <143938697+tianyuzhou668@users.noreply.github.com>
2025-12-05 19:20:50 +0800

    [Iluvatar GPU] Enable bf16, fp16 and sdpa (#4803)
    * [Iluvatar GPU] Enable bf16, fp16 and sdpa
    
    * [Iluvatar GPU] Update
    
    * [Iluvatar GPU] Update

--- paddlex/inference/models/doc_vlm/modeling/paddleocr_vl/_siglip.py
+++ paddlex/inference/models/doc_vlm/modeling/paddleocr_vl/_siglip.py
@@ -42,7 +42,7 @@ import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
 
-from ......utils.env import get_gpu_compute_capability
+from ......utils.env import get_gpu_compute_capability, get_device_type
 from ....common.transformers.activations import ACT2FN
 from ....common.transformers.transformers import PretrainedModel
 from ....common.transformers.transformers.model_outputs import (
@@ -140,6 +140,8 @@ class SiglipAttention(nn.Layer):
 
         cap = get_gpu_compute_capability()
         self._supports_sdpa = cap >= (8, 0) if cap is not None else False
+        if get_device_type() == 'iluvatar_gpu':
+            self._supports_sdpa = True
 
     def forward(
         self,
