b4304fbabd39e5820ba5b046135de06a341a87f0
Lin Manhui <bob1998425@hotmail.com>
2025-12-09 15:06:01 +0800

    Remove debugging env (#4816)

--- deploy/hps/sdk/scripts/assemble.sh
+++ deploy/hps/sdk/scripts/assemble.sh
@@ -4,7 +4,6 @@ docker run \
     -it \
     -e OUID="$(id -u)" \
     -e OGID="$(id -g)" \
-    -e PIP_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple \
     -v "$(pwd)":/workspace \
     -w /workspace \
     --rm \
