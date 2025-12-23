fac1ac3ec85a3677a23e7cf5f966ef8d7ddf9fef
SpongeBob0318 <204268140@qq.com>
2025-12-23 04:09:04 +0000

    allow footer_image between two table

--- paddlex/inference/pipelines/layout_parsing/merge_table.py
+++ paddlex/inference/pipelines/layout_parsing/merge_table.py
@@ -156,7 +156,7 @@ def can_merge_tables(prev_page, prev_block, curr_page, curr_block):
 
     prev_index = prev_page.index(prev_block)
     allowed_follow = all(
-        b.label in ["footer", "vision_footnote", "number", "footnote"]
+        b.label in ["footer", "vision_footnote", "number", "footnote", "footer_image"]
         for b in prev_page[prev_index + 1 :]
     )
     if not allowed_follow:
