a903b4a820cb1ae85fbd1bf386963ca84b7196aa
zhangyue66 <zhangyue66@baidu.com>
2026-01-05 18:51:47 +0800

    local model add min_pixels, max_pixels

--- paddlex/inference/models/doc_vlm/predictor.py
+++ paddlex/inference/models/doc_vlm/predictor.py
@@ -200,7 +200,9 @@ class DocVLMPredictor(BasePredictor):
         if self._use_local_model:
             src_data = copy.copy(data)
             # preprocess
-            data = self.processor.preprocess(data)
+            data = self.processor.preprocess(
+                data, min_pixels=min_pixels, max_pixels=max_pixels
+            )
             data = self._switch_inputs_to_device(data)
 
             # do infer
@@ -220,14 +222,6 @@ class DocVLMPredictor(BasePredictor):
             if top_p is not None:
                 warnings.warn(
                     "`top_p` is currently not supported by the local model and will be ignored."
-                )
-            if min_pixels is not None:
-                warnings.warn(
-                    "`min_pixels` is currently not supported by the local model and will be ignored."
-                )
-            if max_pixels is not None:
-                warnings.warn(
-                    "`max_pixels` is currently not supported by the local model and will be ignored."
                 )
             if use_cache is not None:
                 generate_kwargs["use_cache"] = use_cache
