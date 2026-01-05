fe972ba138ca33987bcf3a5338a5878e7664acbe
Jiaxin Sui <95567040+plusNew001@users.noreply.github.com>
2026-01-05 10:36:15 +0800

    Change paddlepaddle-xpu installation method
    Updated pip install command for paddlepaddle-xpu to use a specific wheel file.

--- tests/run_xpu_ci.sh
+++ tests/run_xpu_ci.sh
@@ -3,7 +3,7 @@ tar -xf PaddleTest.tar && rm -rf  PaddleTest.tar
 xpu-smi 
 python --version
 python -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-python -m pip install --pre paddlepaddle-xpu -i https://www.paddlepaddle.org.cn/packages/nightly/xpu-p800/
+python -m pip install https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-TagBuild-Training-Linux-Xpu-P800-SelfBuiltPypiUse/latest/paddlepaddle_xpu-0.0.0-cp310-cp310-linux_x86_64.whl
 python -c "import paddle; paddle.version.show()"
 cp -r PaddleTest/models/PaddleX/ci ./
 export DEVICE_ID=${cudaid2}
