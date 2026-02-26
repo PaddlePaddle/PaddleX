3d5e3a0ade3e165e5049ef8b91d4e77014a40c0a
Lin Manhui <bob1998425@hotmail.com>
2026-02-26 11:12:52 +0800

    Use git hash as image version (#5016)

--- deploy/hps/server_env/scripts/remove_images.sh
+++ deploy/hps/server_env/scripts/remove_images.sh
@@ -3,9 +3,8 @@
 paddlex_version="$(cat ../../../paddlex/.version)"
 
 for device_type in 'gpu' 'cpu'; do
-    version="$(cat "${device_type}_version.txt")"
     docker rmi \
         "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlex/hps:paddlex${paddlex_version%.*}-${device_type}" \
-        "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlex/hps:${version}-paddlex${paddlex_version}-${device_type}" \
+        "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlex/hps:$(git rev-parse --short HEAD)-${device_type}" \
         "ccr-2vdh3abv-pub.cnc.bj.baidubce.com/paddlex/hps:latest-${device_type}"
 done
