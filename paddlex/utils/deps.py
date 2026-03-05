7e066b58cdf909192c81d1f1c8206c2da6151ddc
zhangyue66 <zhangyue66@baidu.com>
2026-03-05 17:54:31 +0800

    update

--- paddlex/utils/deps.py
+++ paddlex/utils/deps.py
@@ -282,9 +282,10 @@ def is_genai_engine_plugin_available(backend="any"):
             if is_cuda_available():
                 if "vllm" in backend:
                     vllm_version = get_dep_version("vllm")
-                    if vllm_version is not None and Version(vllm_version) >= Version(
-                        "0.12.0"
-                    ):
+                    assert (
+                        vllm_version is not None
+                    ), "Could not find vLLM. Please install it by running: 'paddlex --install genai-vllm-server'"
+                    if Version(vllm_version) >= Version("0.12.0"):
                         return True
                 return is_dep_available("xformers") and is_dep_available("flash-attn")
             return True
