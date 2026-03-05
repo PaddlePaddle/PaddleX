af088471bd140c85473b8aa17e6037faff042e9d
liu-jiaxuan <jxliu1999@163.com>
2026-03-05 12:46:23 +0000

    refine codes

--- paddlex/inference/models/object_detection/modeling/rt_detr.py
+++ paddlex/inference/models/object_detection/modeling/rt_detr.py
@@ -239,6 +239,10 @@ class RTDETRConfig(PretrainedConfig):
         eos_coefficient=1e-4,
         **kwargs,
     ):
+        if backbone_config["model_type"] != "hgnet_v2":
+            raise RuntimeError(
+                f"There is no dynamic graph implementation for backbone {repr(backbone_config["model_type"])}."
+            )
         self.arch = backbone_config["arch"]
         self.freeze_stem_only = backbone_config["freeze_stem_only"]
         self.freeze_at = backbone_config["freeze_at"]
