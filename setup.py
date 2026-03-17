08eb8dd57bb615d429628a5c71873036ddbe85ca
Bobholamovic <mhlin425@whu.edu.cn>
2026-03-17 05:56:26 +0000

    Update paddle2onnx version

--- setup.py
+++ setup.py
@@ -235,7 +235,7 @@ EXTRAS = {
             "vllm == 0.10.2",
         ],
         "paddle2onnx": [
-            "paddle2onnx == 2.0.2rc3",
+            "paddle2onnx == 2.1.0",
         ],
         "serving": [
             "aiohttp >= 3.9",
