731b236d104e695f29e29cef103c33443193404a
Lin Manhui <bob1998425@hotmail.com>
2026-03-26 18:55:44 +0800

    Fix paddle shape bug (#5067)

--- paddlex/inference/models/common/transformers/transformers/conversion_utils.py
+++ paddlex/inference/models/common/transformers/transformers/conversion_utils.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Dict, List, Optional, TypeVar
 
