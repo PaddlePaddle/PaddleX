0030d3f20316d8694c068f4f9a3c6cb1d71141b4
zhangyue66 <zhangyue66@baidu.com>
2026-03-26 21:56:39 +0800

    fix

--- paddlex/inference/models/common/transformers/activations.py
+++ paddlex/inference/models/common/transformers/activations.py
@@ -156,7 +156,6 @@ ACT2CLS = {
     "gelu_10": (ClippedGELUActivation, {"min": -10, "max": 10}),
     "gelu_fast": FastGELUActivation,
     "gelu_new": NewGELUActivation,
-    "hardswish": nn.Hardswish,
     # HACK
     "gelu_pytorch_tanh": NewGELUActivation,
     "gelu_python": (GELUActivation, {"use_gelu_python": True}),
