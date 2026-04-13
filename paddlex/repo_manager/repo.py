f8371c1c0d782f551af9c23a6ec3dcdc0abc20ed
zhang-prog <69562787+zhang-prog@users.noreply.github.com>
2026-04-13 11:33:56 +0800

    fix nlp dep (#5080)
    * Update paddlenlp branch to release/3.0
    
    * skip tool_helpers
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Bobholamovic <bob1998425@hotmail.com>

--- paddlex/repo_manager/repo.py
+++ paddlex/repo_manager/repo.py
@@ -399,6 +399,9 @@ class RepositoryGroupInstaller(object):
             elif req.name == "imgaug":
                 # HACK
                 line_s = "imgaug @ https://paddle-model-ecology.bj.bcebos.com/paddlex/PaddleX3.0/patched_packages/imgaug-0.4.0%2Bpdx-py2.py3-none-any.whl"
+            elif "tool_helpers" in req.name:
+                # For compatibility with higher versions of Python (python>=3.12)
+                continue
             lines.append(line_s)
 
         return "\n".join(lines)
